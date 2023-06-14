52699171478af3cbb589d806a4904814ff1f0bbc
willfineberg <113216135+willfineberg@users.noreply.github.com>
2023-06-13 19:46:59 -0500

    Modified pdf_scraper.py to batch PDFs

--- mcdc_tif_research/pdf_scraper.py
+++ mcdc_tif_research/pdf_scraper.py
@@ -1,14 +1,25 @@
-import tabula as tb
+import os
 import pandas as pd
 import pdfquery
+import tabula
 
-pdf = pdfquery.PDFQuery('T_052_KinzieAR21.pdf')
-pdf.load(5)
-pdf.tree.write('pdfXML.txt', pretty_print = True)
+# Directory of PDFs to produce XML from
+pdfDir = r"c:\sc\pdf"
+xmlDir = r"c:\sc\xml"
 
-cumulative_totals = pdf.pq('LTTextLineHorizontal:overlaps_bbox("468.52, 392.733, 524.154, 402.718")').text()
-# page = pd.DataFrame({'cumulative_totals': cumulative_totals}, index=[0])
-print(cumulative_totals)
+# Loop each PDF in pdfDir
+for filename in os.listdir(pdfDir):
+    if filename.endswith('.pdf'):
+        # Obtain the PDFQuery Object and load it
+        pdfPath = os.path.join(pdfDir, filename)
+        pdf = pdfquery.PDFQuery(pdfPath)
+        pdf.load(5) # Page 6
 
-    
+        # Write the loaded data into XML
+        xml_path = os.path.join(xmlDir, os.path.basename(pdfPath).replace('.pdf', '.xml'))
+        pdf.tree.write(xml_path, pretty_print=True)
 
+        # Post-processing of XML elements
+        cumulative_totals = pdf.pq('LTTextLineHorizontal:overlaps_bbox("468.52, 392.733, 524.154, 402.718")').text()
+        # page = pd.DataFrame({'cumulative_totals': cumulative_totals}, index=[0])
+        print(cumulative_totals)
