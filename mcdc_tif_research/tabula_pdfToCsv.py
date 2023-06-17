3ee76219bf6d540d7596d38d19365b065d8df305
willfineberg <113216135+willfineberg@users.noreply.github.com>
2023-06-17 13:48:15 -0500

    Build a CSV Row for each TIF

--- mcdc_tif_research/tabula_pdfToCsv.py
+++ mcdc_tif_research/tabula_pdfToCsv.py
@@ -16,9 +16,17 @@ from bs4 import BeautifulSoup  # For HTML parsing the DAR URLs
 #                                  #  
 #                                  #
 
-# FUNCTION: String to Float
-# RETURNS: Numerical Value as Float
 def stof(toClean):
+    """
+    Converts a string to a float.
+    
+    Args:
+        toClean (str): The string to be converted.
+    
+    Returns:
+        float: The numerical value as a float.
+    """
+
     # Handle zeroes (which are represented as dashes)
     toClean = toClean.replace('-', '0')
     # Handle bad characters that may or may not occur
@@ -27,22 +35,61 @@ def stof(toClean):
     # Return the cleaned string as a Float
     return locale.atof(toClean)
 
-# FUNCTION: Obtains a list of URLs using BeautifulSoup
-# RETURNS: List of URLs
-def urlList():
-    # Load 2021 TIF reports url
-    url = "https://www.chicago.gov/city/en/depts/dcd/supp_info/district-annual-reports--2021-.html"
+def urlList(url):
+    """
+    Obtains a list of URLs using BeautifulSoup.
+    
+    Returns:
+        list: A list of URLs.
+    """
+
+    # Load 2021 TIF reports URL
     r = requests.get(url)
-    # Parses through html
+    # Parses through HTML
     soup = BeautifulSoup(r.text, "html.parser")
     links = soup.find_all(href=True) #contains hyperlink
-    # Return a List of pdf links
+    # Return a List of PDF links
     pdf_links = ["https://www.chicago.gov" + link['href'] for link in soup.find_all(href=lambda href: href and href.endswith('.pdf'))]
     return pdf_links
 
-# FUNCTION: Convert the first data table from the report (page 6) into a CSV using Tabula
-# RETURNS: Dictionary with TIF ID Number as Integer and CSV Filepath as String
-def urlToCsv(url, outDir):
+def getNameYear_p6(url):
+    """
+    Obtains the Name and Year of a TIF from a PDF URL.
+    
+    Args:
+        url (str): The URL of the PDF.
+    
+    Returns:
+        str: The name of the TIF as a string.
+        str: The year of the TIF as a string.
+    """
+
+    # Makes a Dataframe out of the top part of Page 6
+    df = tabula.read_pdf(
+        input_path=url,
+        pages="6", 
+        area=[65, 0, 105, 600], # [topY, leftX, bottomY, rightX]
+        pandas_options={'header': None},
+    )
+    # Obtains the Name and Year by table location
+    tifName = df[0].iloc[1,1].replace(" Redevelopment Project Area", "")
+    tifYear = df[0].iloc[0,0].split()[-1]
+    # Return name, year
+    return tifName, tifYear
+
+def getData_p6(url, outDir):
+    """
+    Converts the first data table from the report (page 6) into a CSV using Tabula.
+    
+    Args:
+        url (str): The URL of the PDF.
+        outDir (str): The directory path to store the extracted CSV.
+    
+    Returns:
+        int: The TIF ID number.
+        str: The filepath of the extracted CSV.
+    """
+
     # Obtain ID from URL
     filename = url.split("/")[-1]
     pattern = r"T_(\d+)_"
@@ -60,15 +107,23 @@ def urlToCsv(url, outDir):
         input_path=url,
         output_path=outFp,  # modify this for batch processing
         pages="6", 
-        area=[130, 45, 595, 585], # [top, left, bottom, right]
+        area=[130, 45, 595, 585], # [topY, leftX, bottomY, rightX]
         # columns=[45, 362.43, 453.04, 528.64],
     )
-    # Return the URL ID and CSV Filepath
-    return {'id': idNum, 'fp': outFp}
+    # Return the URL ID Integer and CSV Filepath String
+    return idNum, outFp
 
-# FUNCTION: Cleans the CSV, overwrites it, and loads it back into a Pandas DataFrame
-# RETURNS: DataFrame
 def cleanCsv(csvFp):
+    """
+    Cleans the CSV, overwrites it, and loads it back into a Pandas DataFrame.
+    
+    Args:
+        csvFp (str): The filepath of the CSV.
+    
+    Returns:
+        pandas.DataFrame: The cleaned DataFrame.
+    """
+
     # Open the CSV produced by Tabula
     with open(csvFp, 'r') as file:
         reader = csv.reader(file)
@@ -100,23 +155,35 @@ def cleanCsv(csvFp):
     # Return the DataFrame
     return df
 
-# FUNCTION: Sets Dictionary Values by parsing the CSV
-# RETURNS: Output Dictionary
-def putDataInDict(df, id):
+def csvDataToDict(df, id, name, year):
+    """
+    Sets dictionary values by parsing the CSV.
+    
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the CSV data.
+        id (int): The TIF ID number.
+        name (str): The name of the TIF.
+        year (str): The year of the TIF.
+    
+    Returns:
+        dict: The output dictionary.
+    """
+
     # Set Locale for use of atoi() when parsing data
     locale.setlocale(locale.LC_NUMERIC, 'en_US.UTF-8')
     # Create a Dictionary to store our desired values
     outDict = {
-        'tif_name': '',
+        'tif_year': year,
+        'tif_name': name,
         'tif_number': '',
-        '2022_property_tax_extrction': '',
+        f'{year}_property_tax_extrction': '',
         'cumulative_property_tax_extraction': '',
-        '2022_transfers_in': '',
+        f'{year}_transfers_in': '',
         'cumulative_transfers_in': '',
-        '2022_expenses': '',
-        'fund_balance_end_2022': '',
-        '2022_transfers_out': '',
-        '2022_distribution': '',
+        f'{year}_expenses': '',
+        f'fund_balance_end_{year}': '',
+        f'{year}_transfers_out': '',
+        f'{year}_distribution': '',
         'admin_costs': '',
         'finance_costs': '',
         'bank': ''
@@ -131,7 +198,7 @@ def putDataInDict(df, id):
     propTaxIncCur = propTaxIncRow['Revenue/Cash Receipts for Current Reporting Year'].values[0]
     propTaxIncCum = propTaxIncRow['Cumulative Totals of Revenue/Cash Receipts for life of TIF'].values[0]
     # Use the user-defined stof() to clean the Strings to Integers for storage in outDict
-    outDict['2022_property_tax_extrction'] = stof(propTaxIncCur)
+    outDict[f'{year}_property_tax_extrction'] = stof(propTaxIncCur)
     outDict['cumulative_property_tax_extraction'] = stof(propTaxIncCum)
 
     # Obtain the Pandas series for the 'Transfers from Municipal Sources' Row
@@ -140,7 +207,7 @@ def putDataInDict(df, id):
     transFromMunCur = transFromMunRow['Revenue/Cash Receipts for Current Reporting Year'].values[0]
     transFromMunCum = transFromMunRow['Cumulative Totals of Revenue/Cash Receipts for life of TIF'].values[0]
     # Use the user-defined stof() to clean the Strings to Integers for storage in outDict
-    outDict['2022_transfers_in'] = stof(transFromMunCur)
+    outDict[f'{year}_transfers_in'] = stof(transFromMunCur)
     outDict['cumulative_transfers_in'] = stof(transFromMunCum)
 
     # Obtain the Pandas series for the 'Total Expenditures/Cash Disbursements' Row
@@ -148,50 +215,75 @@ def putDataInDict(df, id):
     # Obtain the value as a String
     totExp = totExpRow['Revenue/Cash Receipts for Current Reporting Year'].values[0]
     # Use the user-defined stof() to clean the String to an Integer for storage in outDict
-    outDict['2022_expenses'] = stof(totExp)
+    outDict[f'{year}_expenses'] = stof(totExp)
 
     # Obtain the Pandas series for the 'FUND BALANCE, END OF REPORTING PERIOD*' Row
     fundBalRow = df[df['SOURCE of Revenue/Cash Receipts:'] == 'FUND BALANCE, END OF REPORTING PERIOD*']
     # Obtain the value as a String
     fundBal = fundBalRow['Revenue/Cash Receipts for Current Reporting Year'].values[0]
     # Use the user-defined stof() to clean the String to an Integer for storage in outDict
-    outDict['fund_balance_end_2022'] = stof(fundBal)
+    outDict[f'fund_balance_end_{year}'] = stof(fundBal)
 
     # Obtain the Pandas series for the 'Transfers to Municipal Sources' Row
     transToMunRow = df[df['SOURCE of Revenue/Cash Receipts:'] == 'Transfers to Municipal Sources']
     # Obtain the value as a String
     transToMun = transToMunRow['Revenue/Cash Receipts for Current Reporting Year'].values[0]
     # Use the user-defined stof() to clean the String to an Integer for storage in outDict
-    outDict['2022_transfers_out'] = stof(transToMun)
+    outDict[f'{year}_transfers_out'] = stof(transToMun)
 
     # Obtain the Pandas series for the 'Total Expenditures/Cash Disbursements' Row
     distSurpRow = df[df['SOURCE of Revenue/Cash Receipts:'] == 'Distribution of Surplus']
     # Obtain the value as a String
     distSurp = distSurpRow['Revenue/Cash Receipts for Current Reporting Year'].values[0]
     # Use the user-defined stof() to clean the String to an Integer for storage in outDict
-    outDict['2022_distribution'] = stof(distSurp)
+    outDict[f'{year}_distribution'] = stof(distSurp)
 
     # Return the finalized Dictionary
     return outDict
 
 #                                 #
 #                                 #
-#        PROCEDURAL SCRIPT        #
-#                                 #  
+#        BELOW IS THE SCRIPT      #
+#   SCRIPT USES FUNCTIONS ABOVE   #  
 #                                 #
+#                                 #
+
+# MODIFY THIS: Filepath to write finalDict data to for each url
+csvFp = r'c:\sc\out.csv'
+
+# DAR URLs to Parse
+url2021 = "https://www.chicago.gov/city/en/depts/dcd/supp_info/district-annual-reports--2021-.html"
+url2020 = "https://www.chicago.gov/city/en/depts/dcd/supp_info/district-annual-reports--2020-.html"
+
+# Create a list to store the dictionaries for each TIF
+dictList = []
 
 # Create a Temporary Directory to store the temporary CSV files
 with tempfile.TemporaryDirectory() as tempDir:
     # Iterate each TIF DAR URL
-    for url in urlList():
-        # Get the TIF ID and the Filepath to the extracted CSV
-        idAndFp = urlToCsv(url, tempDir)
-        # Separate ID and FP to two variables
-        id = idAndFp['id']
-        fp = idAndFp['fp']
+    for url in urlList(url2020):
+        # Get the TIF ID and the Filepath to the extracted CSV (for Page 6 TABLE)
+        id, fp = getData_p6(url, tempDir)
+        # Get the TIF Name and Year (from Page 6)
+        name, year = getNameYear_p6(url)
         # Pass Filepath to cleanCsv()
         df = cleanCsv(fp)
         # Parse the Data and retrieve the Dictionary of desired values
-        finalDict = putDataInDict(df, id)
+        curDict = csvDataToDict(df, id, name, year)
         # Print structured output to console
-        print(json.dumps(finalDict, indent=4))
+        dictList.append(curDict)
+        print(json.dumps(curDict, indent=4))
+
+# Get the list of keys from the first dictionary in the list
+fieldnames = list(dictList[0].keys())
+
+# Write the data to the CSV file
+with open(csvFp, 'w', newline='') as csvfile:
+    # Open the CSV and pass the fieldnames
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # Write the header row (uses fieldnames)
+    writer.writeheader()
+    # Write the data rows (each dict in dictList is one TIF)
+    for dict in dictList:
+        writer.writerow(dict)
+    
