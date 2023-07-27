a1dbdcc95a4366000fd1b4803e8f62d519b0dd53
willfineberg <113216135+willfineberg@users.noreply.github.com>
2023-07-26 20:19:59 -0500

    termtable

--- TIF_PDF_Parsing/termTableParse.py
+++ TIF_PDF_Parsing/termTableParse.py
@@ -5,13 +5,13 @@ url = 'https://www.chicago.gov/content/dam/city/depts/dcd/tif/14reports/T_072_24
 
 # * Obtain Full PDF, split first 4 pages into their own BytesIO object
 pdf = io.BytesIO(requests.get(url).content)
-fullPdf = PyPDF2.PdfReader(pdf)
-pdfWriter = PyPDF2.PdfWriter()
-for pageNum in range(4): # ? - May need to modify this too? Table could take up more or less than 4 pages.
-    pdfWriter.add_page(fullPdf.pages[pageNum])
-# First 4 pages obtained, now write them into bytes
-tt_bytes = io.BytesIO()
-pdfWriter.write(tt_bytes)
+# fullPdf = PyPDF2.PdfReader(pdf)
+# pdfWriter = PyPDF2.PdfWriter()
+# for pageNum in range(4): # ? - May need to modify this too? Table could take up more or less than 4 pages.
+#     pdfWriter.add_page(fullPdf.pages[pageNum])
+# # First 4 pages obtained, now write them into bytes
+# tt_bytes = io.BytesIO()
+# pdfWriter.write(tt_bytes)
 
 # TODO: If we need OCR, implement it here
 # ocr code goes here
